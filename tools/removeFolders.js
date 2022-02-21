5d916c8957139f5a669a92b987e8163dddfc714f
Kevin Schmidt <kevin@kswebentwicklung.de>
2022-02-21 15:12:55 +0100

    Updated dep fs remove.

--- tools/removeFolders.js
+++ tools/removeFolders.js
@@ -1,12 +1,12 @@
-const fs = require('fs');
+const fs = require("fs");
 
 module.exports = {
-	removeDevFolders: (projectPath) => {
-		return new Promise((resolve, reject) => {
-			fs.rmdir(projectPath + '/.git', { recursive: true }, (err) => {
-				if (err) reject(err);
-				resolve(true);
-			});
-		});
-	},
+  removeDevFolders: (projectPath) => {
+    return new Promise((resolve, reject) => {
+      fs.rm(projectPath + "/.git", { recursive: true }, (err) => {
+        if (err) reject(err);
+        resolve(true);
+      });
+    });
+  },
 };
